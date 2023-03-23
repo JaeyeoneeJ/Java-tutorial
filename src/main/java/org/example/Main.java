967aa1563df0c00751948384caab8beb5cb384c8
jaeyeoneej <5ikve@naver.com>
2023-03-23 14:30:21 +0900

    변수 할당해보기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,14 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // 세미콜론(;)을 문장 끝에 붙이지 않으면 컴파일 오류 남 ㅋ
+        int num = 5; // 정수형 변수 선언
+        System.out.println(num);
+
+        String string = "Hello World!"; // 문자형 변수 선언
+        System.out.println(string);
+
+        string = "Goodbye World!"; // 이미 선언된 변수에 다른 값을 할당
+        System.out.println(string);
     }
 }
