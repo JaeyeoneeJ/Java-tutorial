c6831b5a8f6d9f813248403ec185412f2c7c0cb5
jaeyeoneej <5ikve@naver.com>
2023-03-23 17:46:26 +0900

    조건문 & 반복문 퀴즈

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,54 +1,30 @@
 public class Main {
 
     public static void main(String[] args) {
-        // 반복문
-        // for문
-        int sum = 0;
-        for (int i = 0; i < 10; i++) {
-            sum += (i + 1);
-        }
-        System.out.println(sum);
-
-        //  for-each문
-        String[] days = {"월", "화", "수", "목", "금", "토", "일"};
-        for (String day : days) {
-            System.out.println(day);
-        }
-
-        // while문
-        int i = 0;
-        int newSum = 0;
-        while (i < 10) {
-            newSum += i + 1;
-            i++;
-        }
-        System.out.println(newSum);
-
-        // while문 + break
-        i = 0;
-        while (i < 10) {
-            if (i == 5) {
-                break;
-            }
-            i++;
-        }
-        System.out.println(i);
-
-        // for + continue
-        for (int j = 0; j < 10; j++) {
-            if ( j == 5 ) {
-                continue;
-            }
-            System.out.println(j);
-        }
-
-        // do-while 문
-        int k = 1;
+        // 반복문 quiz-1 : 1부터 100까지의 합을 구하는 반복문 코드를 작성해보자
         int result = 0;
-        do {
-            result += k;
-            k += 1;
-        } while ( k < 2); // 조건문 타기 전에 앞의 코드를 먼저 한 번 실행하고 비교함
-        System.out.println(result);
+        for (int i = 0; i < 100; i++) {
+            result += i;
+        }
+        System.out.println("quiz-1: " + result);
+
+        // 반복문 quiz-2: 5초부터 시작하는 카운트다운을 반복문을 사용해서 만들어보자
+        for (int i = 5; i >= 0; i--) {
+            System.out.println("quiz-2: 카운트다운: " + i);
+        }
+
+        // 조건문 & 반복문 quiz-3: 1부터 30까지 홀수의 합과 짝수의 합을 구하고 각각 출력하게 하기
+        int sumOddNum = 0;
+        int sumEvenNum = 0;
+
+        for (int i = 1; i <= 30; i++) {
+            if (i % 2 == 0) {
+                sumEvenNum += i;
+            } else {
+                sumOddNum += i;
+            }
+        }
+        System.out.println("quiz-3: 홀수 합: "+sumOddNum);
+        System.out.println("quiz-3: 짝수 합: "+sumEvenNum);
     }
 }
