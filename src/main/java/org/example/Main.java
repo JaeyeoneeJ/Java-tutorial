6d9b7d32af5d68a198661417a766ebe102dcc5d0
jaeyeoneej <5ikve@naver.com>
2023-03-23 16:34:41 +0900

    연산자 퀴즈

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,26 +2,15 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        // 관계 연산자
-//        int num1 = 10;
-//        int num2 = 20;
-//        int num3 = 10;
-//
-//        System.out.println(num1 > num2); // 10 > 20
-//        System.out.println(num1 >= num3); // 10 >= 10
-//        System.out.println(num1 < num2); // 10 < 20
-//        System.out.println(num1 <= num2); // 10 <= 20
-//        System.out.println(num1 == num3); // 10 == 10
-//        System.out.println(num1 != num2); // 10 != 20
-
-        // 논리 연산자
-        boolean a = true;
-        boolean b = false;
-
-        System.out.println(a && b); // &&: 둘 다 참일 경우 true, 아닐 경우 false
-        System.out.println(a || b); // ||: 둘 다 거짓일 경우 false, 아닐 경우 true
-        System.out.println(!b); // !: 피연산자의 논리 값을 바꿈(true <=> false)
-
-
+        // quiz: 결과 값 추측해보기
+        int num1 = 10;
+        int num2 = 3;
+        boolean bool1 = true;
+        boolean bool2 = false;
+        num1 += num1;
+        num2 *= num1;
+        System.out.println(num1); // 20
+        System.out.println(num2); // 60
+        System.out.println(bool1 && bool2); // false
     }
 }
