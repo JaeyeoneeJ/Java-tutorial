26e159f9ad85e074968805f9e9779d909fd9a76c
jaeyeoneej <5ikve@naver.com>
2023-03-23 16:20:16 +0900

    산술 연산자 / 대입 연산자

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,14 +2,31 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        // quiz-1 : 문자 변수 'S', 정수형 변수 10을 선언 및 출력해보기
-        char str = 'S';
-        int num = 10;
-        System.out.println("str: "+ str);
-        System.out.println("num: "+ num);
+        // 산술 연산자
+        int num1 = 10;
+        int num2 = 4;
 
-        // quiz-2 : 크기가 3인 정수형 배열을 선언 및 주어진 숫자 3개를 할당하고 배열의 마지막 값을 출력해보기 (숫자 3개 = 10, 20, 30)
-        int[] arr = {10,20,30};
-        System.out.println(arr[arr.length -1]); // 배열 변수 뒤에 .length로 배열 안 아이템의 갯수 파악 가능
+        System.out.println("더하기 연산: " + (num1 + num2));
+        System.out.println("빼기 연산: " + (num1 - num2));
+        System.out.println("곱하기 연산: " + (num1 * num2));
+        System.out.println("나누기 연산: " + (num1 / num2));
+        System.out.println("나머지 연산: " + (num1 % num2));
+
+        // 대입 연산자
+        num1 += num2; // num1 = num1 + num2
+        System.out.println("num1 += num2, num1 = "+num1);
+
+        num1 -= num2; // num1 = num1 - num2
+        System.out.println("num1 -= num2, num1 = "+num1);
+
+        num1 *= num2; // num1 = num1 * num2
+        System.out.println("num1 *= num2, num1 = "+num1);
+
+        num1 /= num2; // num1 = num1 / num2
+        System.out.println("num1 /= num2, num1 = "+num1);
+
+        num1 %= num2; // num1 = num1 % num2
+        System.out.println("num1 %= num2, num1 = "+num1);
+
     }
 }
