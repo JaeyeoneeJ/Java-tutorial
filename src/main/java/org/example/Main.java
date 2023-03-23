c1a5adda0246e21aaa7750f8552f70850bf43352
jaeyeoneej <5ikve@naver.com>
2023-03-23 16:31:39 +0900

    관계 연산자 / 논리 연산자

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,31 +2,26 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        // 산술 연산자
-        int num1 = 10;
-        int num2 = 4;
+        // 관계 연산자
+//        int num1 = 10;
+//        int num2 = 20;
+//        int num3 = 10;
+//
+//        System.out.println(num1 > num2); // 10 > 20
+//        System.out.println(num1 >= num3); // 10 >= 10
+//        System.out.println(num1 < num2); // 10 < 20
+//        System.out.println(num1 <= num2); // 10 <= 20
+//        System.out.println(num1 == num3); // 10 == 10
+//        System.out.println(num1 != num2); // 10 != 20
 
-        System.out.println("더하기 연산: " + (num1 + num2));
-        System.out.println("빼기 연산: " + (num1 - num2));
-        System.out.println("곱하기 연산: " + (num1 * num2));
-        System.out.println("나누기 연산: " + (num1 / num2));
-        System.out.println("나머지 연산: " + (num1 % num2));
+        // 논리 연산자
+        boolean a = true;
+        boolean b = false;
 
-        // 대입 연산자
-        num1 += num2; // num1 = num1 + num2
-        System.out.println("num1 += num2, num1 = "+num1);
+        System.out.println(a && b); // &&: 둘 다 참일 경우 true, 아닐 경우 false
+        System.out.println(a || b); // ||: 둘 다 거짓일 경우 false, 아닐 경우 true
+        System.out.println(!b); // !: 피연산자의 논리 값을 바꿈(true <=> false)
 
-        num1 -= num2; // num1 = num1 - num2
-        System.out.println("num1 -= num2, num1 = "+num1);
-
-        num1 *= num2; // num1 = num1 * num2
-        System.out.println("num1 *= num2, num1 = "+num1);
-
-        num1 /= num2; // num1 = num1 / num2
-        System.out.println("num1 /= num2, num1 = "+num1);
-
-        num1 %= num2; // num1 = num1 % num2
-        System.out.println("num1 %= num2, num1 = "+num1);
 
     }
 }
