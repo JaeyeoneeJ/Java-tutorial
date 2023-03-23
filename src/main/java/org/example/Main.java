00173c0ad96997ea5513020524f34db5021fed5a
jaeyeoneej <5ikve@naver.com>
2023-03-23 16:51:52 +0900

    조건문(if / switch) / 삼항 연산자

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,15 +2,35 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        // quiz: 결과 값 추측해보기
-        int num1 = 10;
-        int num2 = 3;
-        boolean bool1 = true;
-        boolean bool2 = false;
-        num1 += num1;
-        num2 *= num1;
-        System.out.println(num1); // 20
-        System.out.println(num2); // 60
-        System.out.println(bool1 && bool2); // false
+        // if 조건문
+        int check = 100;
+        int num1 = 50;
+        if (num1 > check) {
+            System.out.println("100보다 큰 수입니다.");
+        } else {
+            System.out.println("100보다 작은 수 입니다.");
+        }
+
+        // switsh문
+        char score = 'A';
+        switch (score) {
+            case 'A':
+                System.out.println("A등급입니다.");
+                break;
+            case 'B':
+                System.out.println("B등급입니다.");
+                break;
+            case 'C':
+                System.out.println("C등급입니다.");
+                break;
+            default:
+                System.out.println("C등급보다 낮은 등급입니다.");
+                break;
+        }
+
+        // 삼항 연산자
+        int a = 5;
+        String result = (a < 10) ? "10보다 작습니다." : "10보다 큽니다.";
+        System.out.println(result);
     }
 }
