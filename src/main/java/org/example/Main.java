82db6fa20ee1e94515783d7c9b9295cc0200de44
jaeyeoneej <5ikve@naver.com>
2023-03-23 15:04:47 +0900

    문자 기본 자료형 알아보기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int num = 5; // 정수형 변수 선언
+//        int num = 5; // 정수형 변수 선언
         // 세미콜론(;)을 문장 끝에 붙이지 않으면 컴파일 오류 남 ㅋ
 //        System.out.println(num);
 //
@@ -25,32 +25,37 @@ public class Main {
 //        System.out.println(num2);
         // num2 = 50; // error: cannot assign a value to final variable num2. 상수이기 때문!
 
-        short s = 1;
-        System.out.println(s);
+        // 숫자 기본 자료형 알아보기
+//        short s = 1;
+//        System.out.println(s);
+//
+//        int a = 3; // 정수형 변수 선언
+//        System.out.println(a);
+//
+//        long b = 1234567890L; // Long 정수형 변수 선언
+//        System.out.println(b);
+//
+//        float c = 5.5F; // float 실수형 변수 선언
+//        System.out.println(c);
+//
+//        double d = 9.12345678901234567890d; // double 실수형 변수 선언
+//        System.out.println(d);
+//
+//        // 다음처럼 각 자료형의 MAX, MIN 값을 가져올 수 있음
+//        System.out.println(Short.MAX_VALUE);
+//        System.out.println(Short.MIN_VALUE);
+//        System.out.println(Integer.MAX_VALUE);
+//        System.out.println(Integer.MIN_VALUE);
+//        System.out.println(Long.MAX_VALUE);
+//        System.out.println(Long.MIN_VALUE);
+//        System.out.println(Float.MAX_VALUE);
+//        System.out.println(Float.MIN_VALUE);
+//        System.out.println(Double.MAX_VALUE);
+//        System.out.println(Double.MIN_VALUE);
 
-        int a = 3; // 정수형 변수 선언
-        System.out.println(a);
-
-        long b = 1234567890L; // Long 정수형 변수 선언
-        System.out.println(b);
-
-        float c = 5.5F; // float 실수형 변수 선언
-        System.out.println(c);
-
-        double d = 9.12345678901234567890d; // double 실수형 변수 선언
-        System.out.println(d);
-
-        // 다음처럼 각 자료형의 MAX, MIN 값을 가져올 수 있음
-        System.out.println(Short.MAX_VALUE);
-        System.out.println(Short.MIN_VALUE);
-        System.out.println(Integer.MAX_VALUE);
-        System.out.println(Integer.MIN_VALUE);
-        System.out.println(Long.MAX_VALUE);
-        System.out.println(Long.MIN_VALUE);
-        System.out.println(Float.MAX_VALUE);
-        System.out.println(Float.MIN_VALUE);
-        System.out.println(Double.MAX_VALUE);
-        System.out.println(Double.MIN_VALUE);
-
+        // 문자 기본 자료형 알아보기
+        char alphabet = 'A'; // 문자형 변수 선언
+        System.out.println(alphabet);
+        // char type은 default 값이 없음
     }
 }
