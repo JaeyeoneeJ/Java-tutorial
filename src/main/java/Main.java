cf526462f966340c126fd703ec6644812b50702d
jaeyeoneej <5ikve@naver.com>
2023-03-23 18:03:29 +0900

    메소드 선언과 구현

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,24 @@
-class Phone { // 클래스 선언
-    String model;
-    String color;
-    int price;
+class Calcuation {
+    int add(int x, int y) {
+        int result = x + y;
+        return result;
+    } // 두 값을 더한 결과
+
+    int subtract(int x, int y) {
+        int result = x - y;
+        return result;
+    }
 }
 
 public class Main {
 
     public static void main(String[] args) {
-        // 클래스
-        Phone galaxy = new Phone(); // 같은 클래스로 각기 다른 인스턴스 생성
-        galaxy.model = "Galaxy10";
-        galaxy.color = "Black";
-        galaxy.price = 100;
+        // 메소드 선언과 구현
+        Calcuation calcuation = new Calcuation();
+        int addResult = calcuation.add(100, 90);
+        int subResult = calcuation.subtract(90, 70);
 
-        Phone iphone = new Phone(); // 같은 클래스로 각기 다른 인스턴스 생성
-        iphone.model = "iPhoneX";
-        iphone.color = "White";
-        iphone.price = 200;
-
-        System.out.println("철수는 이번에 " + galaxy.model + " " + galaxy.color + " 색상을 " + galaxy.price + "만원에 샀다.");
-        System.out.println("영희는 이번에 " + iphone.model + " " + iphone.color + " 색상을 " + iphone.price + "만원에 샀다.");
+        System.out.println("두 개를 더한 값은 "+addResult + " 입니다.");
+        System.out.println("두 개를 뺀 값은 "+subResult + " 입니다.");
     }
 }
