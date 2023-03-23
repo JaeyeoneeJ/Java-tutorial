e938117db7744eee2d579730b6b0190983e19ee4
jaeyeoneej <5ikve@naver.com>
2023-03-23 17:22:43 +0900

    조건문 quiz-1 : 입력받은 점수가 무슨 등급인지 출력

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        // 조건문 quiz-1 : 입력받은 점수가 무슨 등급인지 출력
+        // 100~91점: A등급
+        // 90~81점: B등급
+        // 80~71점: C등급
+        // 그 외: F등급
+
+        Scanner sc = new Scanner(System.in);
+        int score = sc.nextInt();
+
+        if (score <= 100 && score > 90) {
+            System.out.println("A등급");
+        } else if (score <= 90 && score > 80) {
+            System.out.println("B등급");
+        } else if (score <= 80 && score > 70) {
+            System.out.println("C등급");
+        } else {
+            System.out.println("F등급");
+        }
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,36 +1,5 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        // if 조건문
-        int check = 100;
-        int num1 = 50;
-        if (num1 > check) {
-            System.out.println("100보다 큰 수입니다.");
-        } else {
-            System.out.println("100보다 작은 수 입니다.");
-        }
 
-        // switsh문
-        char score = 'A';
-        switch (score) {
-            case 'A':
-                System.out.println("A등급입니다.");
-                break;
-            case 'B':
-                System.out.println("B등급입니다.");
-                break;
-            case 'C':
-                System.out.println("C등급입니다.");
-                break;
-            default:
-                System.out.println("C등급보다 낮은 등급입니다.");
-                break;
-        }
-
-        // 삼항 연산자
-        int a = 5;
-        String result = (a < 10) ? "10보다 작습니다." : "10보다 큽니다.";
-        System.out.println(result);
-    }
 }
