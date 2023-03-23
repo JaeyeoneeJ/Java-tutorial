21488de0c5c4354535c61720cd1bf2f41bc5e2aa
jaeyeoneej <5ikve@naver.com>
2023-03-23 14:41:25 +0900

    숫자 기본 자료형 알아보기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,19 +2,55 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        // 세미콜론(;)을 문장 끝에 붙이지 않으면 컴파일 오류 남 ㅋ
         int num = 5; // 정수형 변수 선언
-        System.out.println(num);
-
-        String string = "Hello World!"; // 문자형 변수 선언
-        System.out.println(string);
-
-        string = "Goodbye World!"; // 이미 선언된 변수에 다른 값을 할당
-        System.out.println(string);
-
-        final int num2 = 10; // 상수형 변수 선언하기
-        System.out.println(num2);
+        // 세미콜론(;)을 문장 끝에 붙이지 않으면 컴파일 오류 남 ㅋ
+//        System.out.println(num);
+//
+//        String string = "Hello World!"; // 문자형 변수 선언
+//        System.out.println(string);
+//
+//        string = "Goodbye World!"; // 이미 선언된 변수에 다른 값을 할당
+//        System.out.println(string);
+//
+//        final int num2 = 10; // 상수형 변수 선언하System.out.println(num);
+////
+////        String string = "Hello World!"; // 문자형 변수 선언
+////        System.out.println(string);
+////
+////        string = "Goodbye World!"; // 이미 선언된 변수에 다른 값을 할당
+////        System.out.println(string);
+////
+////        final int num2 = 10; // 상수형 변수 선언하기
+////        System.out.println(num2);기
+//        System.out.println(num2);
         // num2 = 50; // error: cannot assign a value to final variable num2. 상수이기 때문!
+
+        short s = 1;
+        System.out.println(s);
+
+        int a = 3; // 정수형 변수 선언
+        System.out.println(a);
+
+        long b = 1234567890L; // Long 정수형 변수 선언
+        System.out.println(b);
+
+        float c = 5.5F; // float 실수형 변수 선언
+        System.out.println(c);
+
+        double d = 9.12345678901234567890d; // double 실수형 변수 선언
+        System.out.println(d);
+
+        // 다음처럼 각 자료형의 MAX, MIN 값을 가져올 수 있음
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(Long.MIN_VALUE);
+        System.out.println(Float.MAX_VALUE);
+        System.out.println(Float.MIN_VALUE);
+        System.out.println(Double.MAX_VALUE);
+        System.out.println(Double.MIN_VALUE);
 
     }
 }
