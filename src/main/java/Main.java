a81a26b909e4f942fdc15fd0e552b7581aa30be5
jaeyeoneej <5ikve@naver.com>
2023-03-24 10:42:57 +0900

    클래스 상속 공부

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,31 @@
-class DefaultValueTest {
-    byte byteDefaultValue;
-    int intDefaultValue;
-    short shortDefaultValue;
-    long longDefaultValue;
-    float floatDefaultValue;
-    double doubleDefaultValue;
-    boolean booleanDefaultValue;
-    String referenceDefaultValue;
+class Animal {
+    String name;
 
+    public void cry() {
+        System.out.println(name + " is crying.");
+    }
+}
+
+class Dog extends Animal { // 상속 받을 때, 오직 하나의 클래스만을 상속받을 수 있음
+
+    Dog(String name) {
+        this.name = name;
+    }
+
+    public void swim() {
+        System.out.println(name + " is swimming!");
+    }
 }
 public class Main {
 
     public static void main(String[] args) {
-        // 인스턴스 변수의 기본 값
-        DefaultValueTest defaultValueTest = new DefaultValueTest();
-        System.out.println("byte default: "+defaultValueTest.byteDefaultValue);
-        System.out.println("short default: "+defaultValueTest.shortDefaultValue);
-        System.out.println("int default: "+defaultValueTest.intDefaultValue);
-        System.out.println("long default: "+defaultValueTest.longDefaultValue);
-        System.out.println("float default: "+defaultValueTest.floatDefaultValue);
-        System.out.println("double default: "+defaultValueTest.doubleDefaultValue);
-        System.out.println("boolean default: "+defaultValueTest.booleanDefaultValue);
-        System.out.println("reference default: "+defaultValueTest.referenceDefaultValue);
+        // 클래스 상속
+        Dog dog = new Dog("코코");
+        dog.cry();
+        dog.swim();
+
+        Animal dog2 = dog;
+        dog2.cry();
+//        dog2.swim(); // compile error, 자식 타입에 있는 swim()을 호출할 수 없음
     }
 }
