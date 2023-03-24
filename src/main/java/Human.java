1cae7cad7d897b7d30c6e78fcde71994f81589b7
jaeyeoneej <5ikve@naver.com>
2023-03-24 18:21:29 +0900

    객체지향 퀴즈 문제

---
+++ src/main/java/Human.java
@@ -0,0 +1,14 @@
+public class Human {
+    String name;
+    int age;
+    int speed;
+    int x, y;
+
+    public Human(String name, int age, int speed, int x, int y) {
+        this.name = name;
+        this.age = age;
+        this.speed = speed;
+        this.x = x;
+        this.y = y;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,24 @@
-// 인터페이스: 객체의 특정 행동의 특징을 정의하는 간단한 문법, 함수의 특징인 접근 제어자, 리턴 타입, 메소드 이름 만을 정의함(함수의 내용 없음)
+// 객체지향 quiz
+// 요구사항
+// - 사람은 자식, 부모님, 조부모님이 있다.
+// - 모든 사람은 이름, 나이, 현재 장소 정보(x,y 좌표)가 있다.
+// - 모든 사람은 걸을 수 있다. 장소(x,y 좌표)로 이동한다.
+// - 자식과 부모님은 뛸 수 있다. 장소(x,y 좌표)로 이동한다.
+// - 조부모님의 기본 속도는 1이다. 부모의 기본속도는 3, 자식은 5이다.
+// - 뛸때는 속도가 기본 속도 대비 +2 빠르다.
+// - 수영할때는 속도가 기본 속도 대비 +1 빠르다.
+// - 자식만 수영을 할 수 있다. 장소(x,y 좌표)로 이동한다.
 
-interface Bird {
-    void fly(int x, int y, int z);
-}
-
-class Pigeon implements Bird {
-    private int x, y, z;
-
-    @Override
-    public void fly(int x, int y, int z) {
-        printLocation();
-        System.out.println("날아갑니다.");
-        this.x = x;
-        this.y = y;
-        this.z = z;
-        printLocation();
-    }
-    public void printLocation() {
-        System.out.println("현재 위치 (" + x + ", " + y + ", " + z + ")");
-    }
-}
-
+// 조건
+// - 모든 종류의 사람의 인스턴스는 1개씩 생성한다.
+// - 모든 사람의 처음 위치는 x,y 좌표가 (0,0)이다.
+// - 모든 사람의 이름, 나이, 속도, 현재 위치를 확인한다.
+// - 걸을 수 있는 모든 사람이 (1,1) 위치로 걷는다.
+// - 뛸 수 있는 모든 사람은 (2,2) 위치로 뛰어간다.
+// - 수영할 수 있는 모든 사람은 (3, -1) 위치로 수영해서 간다.
 
 public class Main {
     public static void main(String[] args) {
-        Bird bird = new Pigeon();
-        bird.fly(1,2,3);
-//        bird.printLocation(); // compile error
+
     }
 }
