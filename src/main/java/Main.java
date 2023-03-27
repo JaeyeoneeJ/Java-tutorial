c4d12b6f9a495326faf942c7cb2ab5a148f19c32
jaeyeoneej <5ikve@naver.com>
2023-03-27 10:33:11 +0900

    try-with-resource / try-catch 차이

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,28 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        try (FileOutputStream out = new FileOutputStream("text.txt")) {
-            // text.txt file에 Hello Wolrd를 출력
-            out.write("Hello World".getBytes());
+
+    // try-with-resource 형식
+//    public static void main(String[] args) {
+//        try (FileOutputStream out = new FileOutputStream("text.txt")) {
+//            // text.txt file에 Hello Wolrd를 출력
+//            out.write("Hello World".getBytes());
+//            out.flush();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+    // try-catch 형식
+    public static void main(String[] args) throws IOException {
+        FileOutputStream out = new FileOutputStream("test.txt");
+        try {
+            // test.txt file에 Hello World를 출력
+            out.write("Hello World!".getBytes());
             out.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        out.close();
     }
 }
