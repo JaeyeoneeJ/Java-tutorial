10d587ae61c5d863cb35ea6aa8d1506f77024a58
jaeyeoneej <5ikve@naver.com>
2023-03-27 10:48:18 +0900

    날짜와 시간 다루기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,26 @@
-// 예외처리 Quiz
-// divide() 함수는 매개변수(parameter)에 들어오는 값에 따라서 ArithmeticException과 ArrayIndexOutOfBoundsException이 발생할 수 있음
-// 1. throws 키워드를 통해 divide() 함수에서 발생할 수 있는 exception의 종류가 무엇인지 알게 하라
-// 2. Main 함수에서 try-catch 문을 이용해서, 다음 동작을 구현해라
-//  - ArithmeticException이 발생할 때는 잘못된 계산임을 알리는 문구를 출력하라
-//  - ArrayIndexOutOfBoundsException이 발생할 때는 현재 배열의 Index 범위를 알려주는 문구를 출력하라
+// 날짜와 시간 다루기
 
-class ArrayCalculation {
-    int[] arr = {0,1,2,3,4};
-
-    public int divide(int denominatorIndex, int numeratorIndex) throws ArithmeticException, ArrayIndexOutOfBoundsException {
-        return arr[denominatorIndex] / arr[numeratorIndex];
-    }
-}
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class Main {
 
     public static void main(String[] args) {
-        ArrayCalculation arrayCalculation = new ArrayCalculation();
+        System.out.println("now()를 활용하여 생성"); // 현재의 날짜 시간
+        LocalDate date = LocalDate.now();
+        LocalTime time = LocalTime.now();
+        LocalDateTime dateTime = LocalDateTime.now();
 
-        System.out.println("2 / 1 = " + arrayCalculation.divide(2,1));
-        try {
-            System.out.println("1 / 0 = " + arrayCalculation.divide(1,0));
-        } catch (ArithmeticException arithmeticException) {
-            System.out.println("잘못된 계산입니다. " + arithmeticException.getMessage());
-        }
+        System.out.println(date);
+        System.out.println(time);
+        System.out.println(dateTime);
 
-        try {
-            System.out.println("Try to divide using out of index element = " + arrayCalculation.divide(5,0));
-        } catch (ArrayIndexOutOfBoundsException arrayIndexOutOfBoundsException) {
-            System.out.println("잘못된 index 범위로 나누었습니다. 타당 index 범위는 0부터 " + (arrayCalculation.arr.length - 1) + "까지 입니다.");
-        }
+        System.out.println("of()를 활용하여 생성"); // 지정하는 값을 필드에 담음
+        LocalDate newDate = LocalDate.of(2023, 3, 27);
+        LocalTime newTime = LocalTime.of(22, 50, 55);
+
+        System.out.println(newDate);
+        System.out.println(newTime);
     }
 }
