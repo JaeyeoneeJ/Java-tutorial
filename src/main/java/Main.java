ad9576d1a97b1a48ec95682354dc84306c830afa
jaeyeoneej <5ikve@naver.com>
2023-03-27 13:39:01 +0900

    ArrayDeque 예제

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,27 +8,46 @@
 
 // 컬렉션 인터페이스는 컬렉션 클래스에 저장된 데이터를 읽고, 추가하고 삭제하는 등 데이터를 다루는데 기본적인 메소드들을 정의하고 있음
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayDeque;
 
 public class Main {
 
     public static void main(String[] args) {
-        // Stack 예제
-        System.out.println("--  Queue 예제 --");
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(1);
-        queue.add(3);
-        queue.add(5); // Queue에 값 삽입
-        System.out.println(queue);
-        System.out.println(queue.poll()); // Queue에서 객체를 꺼내어 반환
-        System.out.println(queue);
+        // ArrayDeque 예제
+        System.out.println("-- ArrayDeque 예제 --");
+        ArrayDeque<Integer> arrayDeque = new ArrayDeque<>(); // ArrayDeque를 이용한 선언(제네릭스 이용)
+        arrayDeque.addFirst(1);
+        arrayDeque.addFirst(2);
+        arrayDeque.addFirst(3);
+        arrayDeque.addFirst(4); // arrayDeque의 앞에 값을 삽입
 
-        queue.add(7);
-        queue.add(11);
-        queue.add(9);
-        System.out.println(queue);
-        System.out.println(queue.peek()); // Queue에서 삭제 없이 요소를 반환
-        System.out.println(queue);
+        System.out.println(arrayDeque);
+
+        arrayDeque.addLast(0); // arrayDeque의 끝에 값을 삽입
+        System.out.println(arrayDeque);
+
+        arrayDeque.offerFirst(10); // addFirst와 비슷하지만 큐의 크기 문제가 생길 때, offerFirst는 false를, addFirst는 exception을 반환
+        System.out.println(arrayDeque);
+
+        arrayDeque.offerLast(-1); // arrayDeque의 끝에 값을 삽입
+        System.out.println(arrayDeque);
+        System.out.println(arrayDeque.size()); // 7
+
+        System.out.println(arrayDeque.removeFirst()); // 첫번째 값을 제거하면서 그 값을 리턴
+        System.out.println(arrayDeque.removeLast()); // 마지막 값을 제거하면서 그 값을 리턴
+        System.out.println(arrayDeque);
+        System.out.println(arrayDeque.size()); // 5
+
+        System.out.println(arrayDeque.pollFirst()); // 첫번째 값을 반환 및 제거하면서 그 값을 리턴
+        System.out.println(arrayDeque);
+        System.out.println(arrayDeque.size()); // 4
+
+        System.out.println(arrayDeque. pollLast()); // 마지막 값을 반환 및 제거하면서 그 값을 리턴
+        System.out.println(arrayDeque);
+        System.out.println(arrayDeque.size()); // 3
+
+        System.out.println(arrayDeque.peekFirst()); // 첫번째 값을 반환, 제거하지 않음
+        System.out.println(arrayDeque.peekLast()); // 마지막 값을 반환, 제거하지 않음
+        System.out.println(arrayDeque.size()); // 3
     }
 }
