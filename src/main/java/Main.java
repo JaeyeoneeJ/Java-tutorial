4287557c2eca24667423f7207942225b12d4be74
jaeyeoneej <5ikve@naver.com>
2023-03-27 14:36:07 +0900

    스트림4 예제

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,35 +1,13 @@
-import org.apache.commons.lang3.tuple.Pair;
-
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
-
-class Sale {
-    String fruitName;
-    int price;
-    float discount;
-
-    public Sale(String fruitName, int price, float discount) {
-        this.fruitName = fruitName;
-        this.price = price;
-        this.discount = discount;
-    }
-}
 
 public class Main {
 
     public static void main(String[] args) {
-        // 스트림 예제3: map 연산 활용
-        System.out.println("-- 스트림 예제3 --");
-        List<Sale> saleList = Arrays.asList(
-                new Sale("Apple", 5000, 0.05f),
-                new Sale("Grape", 3000, 0.1f),
-                new Sale("Orange", 4000, 0.02f),
-                new Sale("Tangerine", 2000, 0)
-        );
-
-        Stream<Sale> saleStream = saleList.stream();
-        saleStream.map(sale -> Pair.of(sale.fruitName, sale.price * (1-sale.discount)))
-                .forEach(pair -> System.out.println(pair.getLeft() + " 실 구매가: " + pair.getRight() + "원 "));
+        // 스트림 예제4: reducer를 이용한 계산
+        System.out.println("-- 스트림 예제4 --");
+        List<Integer> numArr = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        Integer result = numArr.stream().reduce(0, Integer::sum); // reduce와 sum을 활용하여 1부터 10까지 더하게 됨
+        System.out.println(result);
     }
 }
