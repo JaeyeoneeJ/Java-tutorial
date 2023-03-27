706e6e398f733958bb5c59757a011644935d033b
jaeyeoneej <5ikve@naver.com>
2023-03-27 10:13:55 +0900

    try-catch-finally 작성해보기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,17 @@
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
 public class Main {
     public static void main(String[] args) {
-        try {
-            // 예외가 발생할 가능성이 있는 코드 작성
-        } catch (FileNotFoundException e) {
-            // FileNotFoundException이 밠갱했을 경우, 처리하기 위한 코드 작성
-        } catch (IOException e) {
-            // FileNotFoundException이 아닌 IOException이 발생했을 경우, 이를 처리하기 위한 코드 작성
-        } finally {
-            // 예외의 발생여부에 관계없이 항상 수행되어야 하는 코드 작성
+        int number = 10;
+        int result;
+
+        for (int i = 10; i >= 0; i--) {
+            try {
+                result = number / i;
+                System.out.println(i + ": " + result);
+            } catch (Exception e) {
+                System.out.println("Exception 발생: " + e.getMessage());
+            } finally {
+                System.out.println("항상 실행되는 finally 구문");
+            }
         }
     }
 }
