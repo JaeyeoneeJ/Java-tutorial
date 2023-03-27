2498c1ff65b29bb9b12bc91717d42bd959b93996
jaeyeoneej <5ikve@naver.com>
2023-03-27 13:05:23 +0900

    HashMap 예제

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,44 +8,33 @@
 
 // 컬렉션 인터페이스는 컬렉션 클래스에 저장된 데이터를 읽고, 추가하고 삭제하는 등 데이터를 다루는데 기본적인 메소드들을 정의하고 있음
 
-
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
-        Set<Integer> integerSet = new HashSet<>(); // Collectioin의 자료형에서 primitive 타입은 올 수 없음. primitive 타입에 해당하는 class가 존재하니 그것을 사용
-        integerSet.add(1);
-        integerSet.add(3);
-        integerSet.add(2);
-        integerSet.add(9); // 하나씩 값을 삽입
-        System.out.println(integerSet); // 출력해보면 순서가 지켜지지 않는 것을 확인
+        // HashMap 예제
+        // Hash Map은 키(key)와 값(value)을 하나의 데이터로 저장하는 특징을 가짐
+        // 이를 통하여 해싱(hashing)을 가능하게 하여 데이터를 검색하는데 뛰어난 성능을 보임
+        // 선언 부분의 <> 표시는 제네릭스라는 것으로 key, value에 해당하는 타입을 <> 안에 선언하는 것이라고 이해하면 됨
 
-        Set<String> stringSet = new HashSet<>();
-        stringSet.add("LA");
-        stringSet.add("New York");
-        stringSet.add("LasVegas");
-        stringSet.add("San Francisco");
-        stringSet.add("Seoul");
-        System.out.println(stringSet);
+        System.out.println("--  HashMap 예제 --");
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "apple");
+        map.put(2, "berry");
+        map.put(3, "cherry");
 
-        stringSet.remove("Seoul"); // Seoul을 HashSet에서 제거
-        System.out.println(stringSet);
+        System.out.println(map);
 
-        ArrayList<String> target = new ArrayList<String>();
-        target.add("New York");
-        target.add("LasVegas"); // 제거할 항목을 ArrayList에 삽임
-        stringSet.removeAll(target);
-        System.out.println(stringSet);
+        System.out.println("1st in map: " + map.get(1));
 
-        System.out.println("LA가 포함되어 있나요? " + stringSet.contains("LA")); // LA가 HashSet에 포함되어 있으면 true, 아니면 false
-        System.out.println("LasVegas가 포함되어 있나요? " + stringSet.contains("LasVegas"));
+        map.remove(2);
+        System.out.println(map);
+        System.out.println(map.containsKey(2));
+        System.out.println(map.containsValue("cherry"));
 
-        System.out.println("현재 HashSet의 크기는 : " + stringSet.size() + "입니다."); // HashSet의 크기를 반환
-
-        stringSet.clear(); // HashSet의 모든 아이템들을 삭제
-        System.out.println(stringSet);
-
-
+        map.clear();
+        System.out.println(map);
     }
 }
